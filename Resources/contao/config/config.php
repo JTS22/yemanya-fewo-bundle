e0f447574a8e0b0aeac4faef58dc4602a7914772
Jonas Schroeder <jonas.schroeder22@hotmail.com>
2019-03-20 16:00:03 +0100

    Cleanup

--- Resources/contao/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$GLOBALS['BE_MOD']['content']['fewo_customer_data'] = array(
-    'tables' => ['tl_fewo_customer'],
-    'table' => ['TableWizard', 'importTable'],
-    'list' => ['ListWizard', 'importList']
-);
-
-?>
